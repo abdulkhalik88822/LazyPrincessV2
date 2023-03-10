0b39444a49524147b26a51340b58808370eecb18
abdulkhalik8188 <125822798+abdulkhalik8188@users.noreply.github.com>
2023-03-10 17:44:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,14 +48,14 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/pathan_h')
 MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 
 # Custom Chats
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001804556939))
-FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -10018108062900))
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1001804556939))
+FILE_CHANNEL = int(environ.get('FILE_CHANNEL', '-10018108062900))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 't.me/cynitebackup')
 HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/pathan_h/60')
 
 # Log Channels
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001720210775))
-RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', -1001896609847))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001720210775'))
+RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', '-1001896609847))
 
 # Bot Options
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
